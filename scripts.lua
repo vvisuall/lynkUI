90728674f753cc35a4cc02ae353eaab95107cf5c
lynk <jerryhamilton9080@gmail.com>
2025-08-12 18:06:02 +0100

    Create scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,19 @@
+-- scripts.lua
+return {
+    [286090429] = { -- Arsenal
+        {
+            Title = "Silent Aim", 
+            Description = "Advanced silent aim", 
+            Url = "https://raw.githubusercontent.com/vvisuall/lynkUI/main/scripts/arsenal-silentaim.lua",
+            RequiresKey = true
+        }
+    },
+    ["Universal"] = {
+        {
+            Title = "Fly Script", 
+            Description = "Simple fly script", 
+            Url = "https://raw.githubusercontent.com/vvisuall/lynkUI/main/scripts/universal-fly.lua",
+            RequiresKey = false
+        }
+    }
+}
